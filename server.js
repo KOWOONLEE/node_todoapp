f0a219cc831c1c136da25f941f6f7019e34822fb
KOWOONLEE <hhh3901@gmail.com>
2023-04-19 23:01:08 +0900

    Add : server start

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+app.listen(8080, function () {
+  console.log("listening on 8080");
+});
+app.get("/beauty", function (req, res) {
+  res.send("뷰티 용품 쇼핑할 수 있는 페이지입니다.");
+});
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});

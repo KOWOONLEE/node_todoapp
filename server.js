9adcb9884c7af123edf5fd5d33d6ac22eca8227c
KOWOONLEE <hhh3901@gmail.com>
2023-04-23 22:12:39 +0900

    Rev: mongodb 설치 후 세팅

--- server.js
+++ server.js
@@ -6,6 +6,32 @@ app.use(express.urlencoded({ extended: true }));
 app.listen(8080, function () {
   console.log("listening on 8080");
 });
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://admin:rhdns3901@cluster0.dambb0b.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/beauty", function (req, res) {
   res.send("뷰티 용품 쇼핑할 수 있는 페이지입니다.");
 });

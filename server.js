858546c9d506eb521c9f2e3a0f502078fe11a364
KOWOONLEE <hhh3901@gmail.com>
2023-04-25 11:00:31 +0900

    Rev: mongodb version rev

--- server.js
+++ server.js
@@ -3,42 +3,23 @@ const app = express();
 // const bodyParser = require("body-parser");
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-// app.listen(8080, function () {
-//   console.log("listening on 8080");
-// });
 // "mongodb+srv://admin:rhdns3901@cluster0.dambb0b.mongodb.net/?retryWrites=true&w=majority"
+app.set("view engine", "ejs");
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://admin:rhdns3901@cluster0.dambb0b.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-async function run() {
-  try {
-    await client.connect();
+const MongoClient = require("mongodb").MongoClient;
+let db;
+MongoClient.connect(
+  "mongodb+srv://admin:rhdns3901@cluster0.dambb0b.mongodb.net/?retryWrites=true&w=majority",
+  { useUnifiedTopology: true },
+  function (에러, client) {
+    if (에러) return console.log(에러);
     db = client.db("todoapp");
-    // collection = await db.collection("post");
-    await db
-      .collection("post")
-      .insertOne({ title: "kowoon", contents: 20 }, function (error, result) {
-        console.log("저장완료");
-      });
-    app.listen(8080, function () {
+    //서버띄우는 코드 여기로 옮기기
+    app.listen("8080", function () {
       console.log("listening on 8080");
     });
-  } finally {
-    await client.close();
   }
-}
-run().catch(console.dir);
+);
 
 app.get("/beauty", function (req, res) {
   res.send("뷰티 용품 쇼핑할 수 있는 페이지입니다.");
@@ -53,21 +34,18 @@ app.get("/write", function (req, res) {
 app.post("/add", function (req, res) {
   res.send("전송완료");
   console.log(req.body);
-  async function run() {
-    try {
-      await client.connect();
-      db = client.db("todoapp");
-      await db
-        .collection("post")
-        .insertOne(
-          { title: req.body.title, contents: req.body.title },
-          function (error, result) {
-            console.log("저장완료");
-          }
-        );
-    } finally {
-      await client.close();
+  db.collection("post").insertOne(
+    { title: req.body.title, contents: req.body.contents },
+    function () {
+      console.log("저장완료");
     }
-  }
-  run().catch(console.dir);
+  );
+});
+app.get("/list", function (req, res) {
+  db.collection("post")
+    .find()
+    .toArray(function (error, result) {
+      console.log(result);
+      res.render("list.ejs", { posts: result });
+    });
 });
